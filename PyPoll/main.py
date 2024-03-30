59f632d14f3b7cc524b10460294efcd20813a994
amaniz1st@gmail.com <amaniz1st@gmail.com>
2024-03-30 10:59:16 -0500

    second commit

---
+++ PyPoll/main.py
@@ -0,0 +1,11 @@
+# start here
+import csv
+file_path='PyPoll\Resources\election_data.csv'
+print('project work')
+
+#creat variables
+#open thre file
+# read row in the file
+# add to total votes
+# print result to screen
+# print the result to file
