00fda1b41f5478af4b6c6db7b2996191b53c141c
amaniz1st@gmail.com <amaniz1st@gmail.com>
2024-03-31 19:09:12 -0500

    last touch

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,23 +7,24 @@ print('project work')
 total_votes=0
 candidates=[]
 candidate_votes =[]
-
 winning_candidate =''
 winning_candidate_votes=0
 
 
-#open thre file
+#open the file
 with open (file_path) as election_file:
     csv_file=csv.reader(election_file)
     #skips a row in the file (first row = header row)
     next(csv_file)
+
+    # read every row in the file
     for row in csv_file:
-        #print(row)
         total_votes += 1
         # Ballot ID,County,Candidate
         ballot_id=row[0]
         county=row[1]
         candidate=row[2]
+        
         #check if candidate exist
         if candidate not in candidates:
             candidates.append(candidate)
@@ -32,15 +33,15 @@ with open (file_path) as election_file:
             candidate_id= candidates.index(candidate)
             candidate_votes[candidate_id] += 1
 
-# print result to screen
+# print result to Terminal
 print('---------------------------------')
 print(f'Total Votes = {total_votes}')
 print('---------------------------------')
 print(candidates)
 print(candidate_votes)
+print('---------------------------------')
 
-
-# find thw winning candidate
+# find the winning candidate
 for candidate in candidates:
     current_candidate_votes = candidate_votes[candidates.index(candidate)]
     current_vote_pct= (current_candidate_votes/total_votes )*100
@@ -54,17 +55,14 @@ for candidate in candidates:
     if current_votes> winning_candidate_votes:
         winning_candidate=candidate
         winning_candidate_votes=current_votes
-# read row in the file
-# add to total votes
 
 # print the result to file
 out_file_path='PyPoll\Resources\election_data.txt'
 with open (out_file_path,'w') as file_out:
+    file_out.write('Election Results\n')
     file_out.write('---------------------------------\n')
     file_out.write(f'Total Votes = {total_votes}\n')
     file_out.write('---------------------------------\n')
-    #file_out.write(str(candidates)\n)
-    #file_out.write(str(candidate_votes))
 
     for candidate in candidates:
         current_candidate_votes = candidate_votes[candidates.index(candidate)]
@@ -74,4 +72,4 @@ with open (out_file_path,'w') as file_out:
     file_out.write(f'Winner: {winning_candidate}\n')
     file_out.write('---------------------------------\n')
 
-#
+#End here
